32241837f129d5387838697554064631835fb490
Mirajul Islam <mirajulislam0724@gmail.com>
2022-09-19 12:02:01 +0600

    Psd to Html responsive project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Bx Slider
+
+$(document).ready(function(){
+    $('.slider_area').bxSlider();
+    $('#nav_area').meanmenu({
+      meanMenuContainer: '#mobile_menu',
+      meanScreenWidth: 991,
+    });
+  });
